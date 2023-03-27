96b86204815e690a8cc91af4f1b4a25f8110ae8b
waylon.yuen <waylon.yuen@gosmio.biz>
2023-03-27 18:01:27 +0800

    Add : 添加 MS-VS 解決方案 & 專案

---
+++ Source/game-monster-trainer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GameMonsterTrainer {
+
+    public static class Program {
+        public static void Main(string[] args) {
+            Console.WriteLine("Game Monster Trainer");
+        }
+    }
+
+}
+
